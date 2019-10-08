bb6ce116ea4984a32dd99cc62f63e8d5de109398
Camilla Satte <camillasatte@gmail.com>
2019-10-07 20:19:55 -0400

    i need help to resolve import issue

--- backend/forms/registration.py
+++ backend/forms/registration.py
@@ -1,4 +1,19 @@
-class RegistrationForm():
+from . import validate
+
+
+def testing():
+    res = validate.is_valid_phone("123456")
+    print(res)
+
+
+testing()
+
+
+class User(object):
+
+    def __init__(self, name):
+        self.name = name
+
     firstName = ""
     lastName = ""
     phone = ""
--- backend/forms/validate.py
+++ backend/forms/validate.py
@@ -1,18 +1,21 @@
 import re
 
-def isValidName(name):
+
+def is_valid_name(name):
     name_max_length = 30
     if name == "":
         return False
     return True
 
-def isValidEmail(email):
+
+def is_valid_email(email):
     regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
-    if(re.search(regex, email)):
+    if re.search(regex, email):
         return True
-    else :
+    else:
         return False
 
-def isValidPhone(phone):
-    if len(phone) != 12:
 
+def is_valid_phone(phone):
+    if len(phone) != 12:
+        return False
