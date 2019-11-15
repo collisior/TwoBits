4975380074913b507b3edfc7da3cab2d96fcdee1
Will M <munozwi@bu.edu>
2019-11-14 20:03:57 -0500

    added login function

---
+++ app/src/main/java/com/example/a2bits/ui/login/profileActivity.java
@@ -0,0 +1,17 @@
+package com.example.a2bits.ui.login;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.a2bits.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class profileActivity extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+}
