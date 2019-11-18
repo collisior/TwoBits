2f6f5ed3314022e18b244e7c878b2ef377839c42
Will M <munozwi@bu.edu>
2019-11-18 18:29:34 -0500

    fixed profile layout

--- app/src/main/java/com/example/a2bits/ui/login/profileActivity.java
+++ app/src/main/java/com/example/a2bits/ui/login/profileActivity.java
@@ -29,51 +29,51 @@ public class profileActivity extends AppCompatActivity {
 //        Date: 2019
 //        Code version: 1.0https://www.youtube.com/watch?v=UsXv6VRqZKs
 
-        models = new ArrayList<>();
-        models.add(new Model(R.drawable.redcross, "Charity Interests",
-                "American Red Cross", "@americanredcross"));
-        models.add(new Model(R.drawable.helenkeller, "Charity Interests",
-                "Helen Keller International", "@hellenkellerinternational"));
-
-        adaptor = new Adaptor(models, this);
-        viewPager = findViewById(R.id.viewPager);
-        viewPager.setAdapter(adaptor);
-        viewPager.setPadding(0,0,0,0);
-
-        Integer[] colors_temp = {
-                getResources().getColor(R.color.white),
-                getResources().getColor(R.color.white)
-        };
-
-        colors = colors_temp;
-
-        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                if(position < (adaptor.getCount() -1) && position < (colors.length - 1)){
-                    viewPager.setBackgroundColor(
-
-                            (Integer) argbEvaluator.evaluate(
-                                    positionOffset,
-                                    colors[position],
-                                    colors[position + 1]
-                            )
-                    );
-                }else{
-                    viewPager.setBackgroundColor(colors[colors.length - 1]);
-                }
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
+//        models = new ArrayList<>();
+//        models.add(new Model(R.drawable.redcross, "Charity Interests",
+//                "American Red Cross", "@americanredcross"));
+//        models.add(new Model(R.drawable.helenkeller, "Charity Interests",
+//                "Helen Keller International", "@hellenkellerinternational"));
+//
+//        adaptor = new Adaptor(models, this);
+//        viewPager = findViewById(R.id.viewPager);
+//        viewPager.setAdapter(adaptor);
+//        viewPager.setPadding(0,0,0,0);
+//
+//        Integer[] colors_temp = {
+//                getResources().getColor(R.color.white),
+//                getResources().getColor(R.color.white)
+//        };
+//
+//        colors = colors_temp;
+//
+//        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+//            @Override
+//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//                if(position < (adaptor.getCount() -1) && position < (colors.length - 1)){
+//                    viewPager.setBackgroundColor(
+//
+//                            (Integer) argbEvaluator.evaluate(
+//                                    positionOffset,
+//                                    colors[position],
+//                                    colors[position + 1]
+//                            )
+//                    );
+//                }else{
+//                    viewPager.setBackgroundColor(colors[colors.length - 1]);
+//                }
+//            }
+//
+//            @Override
+//            public void onPageSelected(int position) {
+//
+//            }
+//
+//            @Override
+//            public void onPageScrollStateChanged(int state) {
+//
+//            }
+//        });
 
     }
 }
