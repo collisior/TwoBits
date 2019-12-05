437e5fc8eeeb846390450c34f449617fe861231f
Kaylyn Mok <mhkaylyn@bu.edu>
2019-12-04 19:27:00 -0500

    test

---
+++ app/src/main/java/com/example/a2bits/ui/login/plaidsuccess.java
@@ -0,0 +1,31 @@
+package com.example.a2bits.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.a2bits.R;
+import com.example.a2bits.ui.login.categoriesActivity;
+
+public class plaidsuccess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plaidsuccess);
+    Button nextButton = findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            openProfile();
+        }
+    });
+}
+    public void openProfile(){
+        Intent intent = new Intent(this, profileActivity.class);
+        startActivity(intent);
+    }
+}
